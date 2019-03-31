646628e0f8469452ee6ca73836f882db44140d7e
YulyaKukareko <YulyaKukareko>
2019-03-31 17:18:45 +0300

    Add view

---
+++ src/main/java/by/epam/javawebtraining/kukareko/task5/view/FileRender.java
@@ -0,0 +1,33 @@
+package by.epam.javawebtraining.kukareko.task5.view;
+
+import org.apache.log4j.Logger;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * @author Yulya Kukareko
+ * @version 1.0 31 Mar 2019
+ */
+public class FileRender implements Renderer {
+
+    public static final Logger LOGGER;
+    private static String fileName = "src/main/resources/logic/parking";
+
+    static {
+        LOGGER = Logger.getLogger("FileRenderer");
+    }
+
+    @Override
+    public void render(String data) {
+
+        try (BufferedWriter br = new BufferedWriter(new FileWriter(fileName, true))) {
+
+            br.write(data + "\n");
+            br.flush();
+
+        } catch (IOException ex) {
+            LOGGER.error(ex.getMessage());
+        }
+    }
+}
