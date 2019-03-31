0270e71c5a88948adb558e1f91bf13e98c09a279
YulyaKukareko <YulyaKukareko>
2019-03-31 20:30:33 +0300

    Add logger

--- src/main/java/by/epam/javawebtraining/kukareko/task5/controller/Controller.java
+++ src/main/java/by/epam/javawebtraining/kukareko/task5/controller/Controller.java
@@ -1,7 +1,7 @@
 package by.epam.javawebtraining.kukareko.task5.controller;
 
 import by.epam.javawebtraining.kukareko.task5.model.entity.ParkingPlace;
-import by.epam.javawebtraining.kukareko.task5.model.logic.Parking;
+import by.epam.javawebtraining.kukareko.task5.model.entity.Parking;
 import by.epam.javawebtraining.kukareko.task5.util.generation.CarGeneration;
 import by.epam.javawebtraining.kukareko.task5.util.generation.ParkingPlacesGeneration;
 
