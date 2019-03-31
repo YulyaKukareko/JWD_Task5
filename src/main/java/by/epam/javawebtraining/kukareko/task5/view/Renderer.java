4930e06230a830a2a98375d200e906958340135a
YulyaKukareko <YulyaKukareko>
2019-03-31 20:48:36 +0300

    Add file render

---
+++ src/main/java/by/epam/javawebtraining/kukareko/task5/view/Renderer.java
@@ -0,0 +1,10 @@
+package by.epam.javawebtraining.kukareko.task5.view;
+
+/**
+ * @author Yulya Kukareko
+ * @version 1.0 31 Mar 2019
+ */
+public interface Renderer {
+
+    void render(String message);
+}
